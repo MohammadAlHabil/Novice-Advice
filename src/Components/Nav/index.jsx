67985e84cae5ce6ef2ecb51a23da7632becc34ff
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-04-19 16:32:40 +0300

    create navbar relates #2

---
+++ src/Components/Nav/index.jsx
@@ -0,0 +1,21 @@
+import './Nav.css';
+import logo from '../../images/logo.png';
+export default function Nav({ setOpenModal }) {
+  return (
+    <div className="wrap-nav">
+      <div className="container">
+        <nav className="navbar">
+          <img src={logo} alt="logo" />
+          <button
+            className="create-qr"
+            onClick={() => {
+              setOpenModal(true);
+            }}
+          >
+            Create QR
+          </button>
+        </nav>
+      </div>
+    </div>
+  );
+}
